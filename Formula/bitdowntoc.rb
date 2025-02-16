c99119d43720eef3f965579967f3499c583a1ac9
Github Workflow <workflow@bot.github.com>
2025-02-16 10:16:49 +0000

    chore: update bitdowntoc to 2.3.0

--- Formula/bitdowntoc.rb
+++ Formula/bitdowntoc.rb
@@ -1,8 +1,8 @@
 class Bitdowntoc < Formula
   desc "Markdown Table Of Content (TOC) generator"
   homepage "https://derlin.github.io/bitdowntoc"
-  url "https://github.com/derlin/bitdowntoc/releases/download/v2.2.1/bitdowntoc-jvm-2.2.1.jar"
-  sha256 "3f448894d83312721dc4454fc18b91483b854167c256de74b0319df44a447d6c"
+  url "https://github.com/derlin/bitdowntoc/releases/download/v2.3.0/bitdowntoc-jvm-2.3.0.jar"
+  sha256 "eecc754a5fb610998b7cfc04ec94f34627b0d196ea8f8f80c1bd75424144c7c6"
   license "Apache-2.0"
 
   depends_on "openjdk"
